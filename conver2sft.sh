c5669500b6c1c796577d1d89869ed9365ed23ec4
d8e <3403465822@qq.com>
2023-10-30 12:35:02 +0000

    remove out of date file

--- conver2sft.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nproc-per-node 8 convert_sft.py \
-    --library_path "/mnt/42_store/sbc/bug_detection/build/java.so"\
-    --language java\
-    --dataset_dir "/mnt/42_store/sbc/bug_detection/datasets"\
-    --model_path "/mnt/42_store/sbc/trans-opus-mt-en-zh"\
-    --output_dir "/mnt/42_store/sbc/bug_detection/datasets_new"
